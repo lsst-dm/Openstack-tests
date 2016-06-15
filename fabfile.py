46fe8921f711cb2a8c667fbd1f3c4e1af8656884
Sahand Hariri <sahandha@gmail.com>
2016-06-15 12:09:27 -0500

    adding a test .sh file

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,23 @@
 from fabric.api import *
 from OpenStackVirtualMachine import OSVM
 import time
+import socket
+
+#vm = OSVM("sahand_test")
+
+env.user = 'ubuntu'
+env.hosts = ['141.142.236.177']
+env.key_filename = './acx.key'
 
 def StartInstance():
-    vm = OSVM("sahand_test")
+    #vm = OSVM("sahand_test")
     vm.setNova()
     vm.setHost()
     vm.startInstance()
     vm.assignFLoatingIP()
 
 def StartInstanceFinish():
-    vm = OSVM("sahand_test")
+    #vm = OSVM("sahand_test")
     vm.setNova()
     vm.setHost()
     vm.startInstance()
@@ -18,19 +25,22 @@ def StartInstanceFinish():
     vm.deleteInstance() # This is the differnce
 
 def SetUpMyVM():
-    vm = OSVM("sahand_test")
+    #vm = OSVM("sahand_test")
     vm.setNova()
     vm.setHost()
     vm.startInstance()
     vm.assignFLoatingIP()
 
-    #env.skip_bad_hosts = True
     env.user = 'ubuntu'
     env.host_string = (vm._Host).ip
-    #env.port = 5000
     env.key_filename = './acx.key'
-
+    n = 20
+    print "wait %d seconds" %n
+    time.sleep(n)
     run('touch myTestFile')
+
+def TestConnectionOnly():
+    run('sudo apt-get install wget')
 
 def ConnectToMe():
     env.host_string = '127.0.0.1'
