22ff36158667770b7e545cf1d3ba4b70844559c4
Sahand Hariri <sahandha@gmail.com>
2016-07-19 13:05:35 -0500

    updates

--- fabfile.py
+++ fabfile.py
@@ -19,8 +19,7 @@ def setFabCreds(vm):
     print "Set Fabric env values for authentication."
     env.user = 'ubuntu'
     env.host_string = (vm._Host).ip
-    env.key_filename = './acx.key'
-
+    env.key_filename = '../acx.key'
 
 def setUpMyVM():
     vm=startInstance("sahand")
@@ -29,11 +28,23 @@ def setUpMyVM():
     waitForSSH()
 
     print "Communicating with the remote VM..."
-    put('./StarterScript.sh')
-    put('./Dockerfile')
+    put('./scripts/StarterScript.sh')
+    put('./scripts/Dockerfile')
     put('~/.vim')
     put('~/.vimrc')
     run('source ./StarterScript.sh')
+
+def buildDockerImage():
+    vm=startInstance("ImageBuilder")
+    setFabCreds(vm)
+    local('ssh-keygen -R %s'%env.host_string)
+    waitForSSH()
+    print "Communicating with the remote VM..."
+    put('./scripts/BuildDockerImage.sh')
+    put('./scripts/Dockerfile')
+    put('~/.vim')
+    put('~/.vimrc')
+    run('source ./BuildDockerImage.sh')
 
 def setUpClusterWithDocker():
     mvm = raw_input("What is the name of the master node? ")
@@ -54,7 +65,7 @@ def setUpMasterNodeWithDocker(vmname):
     print "Communicating with the remote VM..."
     put('~/.vim')
     put('~/.vimrc')
-    put('./SetUpDocker.sh')
+    put('./scripts/SetUpDocker.sh')
     run('source ./SetUpDocker.sh')
 
 def setUpSlaveNodeWithDocker(vmname):
@@ -71,8 +82,24 @@ def setUpSlaveNodeWithDocker(vmname):
     print "Communicating with the remote VM..."
     put('~/.vim')
     put('~/.vimrc')
-    put('./SetUpDocker.sh')
+    put('./scripts/SetUpDocker.sh')
     run('source ./SetUpDocker.sh')
+
+# This needs work.
+def setUpDockerImageMaintainer():
+    print "setting up an image maintainer VM..."
+    vm=startInstance(vmname)
+    setFabCreds(vm)
+    local('ssh-keygen -R %s'%env.host_string)
+    waitForSSH()
+
+    print "Communicating with the remote VM..."
+    put('~/.vim')
+    put('~/.vimrc')
+    run('docker pull sahandha/ubuntu')
+    put('./scripts/DockerFile.sh')
+    # put('./scripts/SetUpDocker.sh')
+    # run('source ./SetUpDocker.sh')
 
 def waitForSSH():
     print "Pinging the remote VM for readiness..."
---
+++ scripts/StarterScript.sh
@@ -0,0 +1,115 @@
+# set the hostname in /etc/hosts
+sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost $HOSTNAME/g" /etc/hosts
+
+echo "============ Update and install wget and git ====================="
+
+sudo apt-get -y update
+sudo rm /boot/grub/menu.lst
+sudo update-grub-legacy-ec2 -y
+sudo apt-get dist-upgrade -qq --force-yes
+sudo apt-get  upgrade
+
+sudo apt-get -y install linux-image-generic-lts-trusty
+
+sudo apt-get -y install wget
+sudo apt-get -y install git
+
+echo "=============Set up SSH key pair for localhost==========================="
+ssh-keygen -f .ssh/sshkey.rsa -t rsa -N ''
+cat ~/.ssh/id_rsa.pub >> authorized_keys
+
+
+echo "==========================Isntalling Java================================"
+# Installation of JDK
+sudo apt-get -y install default-jre
+sudo apt-get -y install default-jdk
+
+
+echo "============================Python Libraries============================="
+sudo apt-get install -y python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv
+sudo pip install pandas
+sudo pip install seaborn
+sudo pip install pyfits
+sudo pip install "ipython[notebook]"
+
+
+echo "===============================Scala installation================================="
+# Installation of scala
+wget http://www.scala-lang.org/files/archive/scala-2.11.8.deb
+sudo dpkg -i scala-2.11.8.deb
+
+echo "===========================Download Spark============================"
+# Downloading spark
+wget http://d3kbcqa49mib13.cloudfront.net/spark-1.6.1-bin-hadoop2.6.tgz
+tar -zxf spark-1.6.1-bin-hadoop2.6.tgz
+cd spark-1.6.1-bin-hadoop2.6/conf
+cp log4j.properties.template log4j.properties
+sed -i 's/log4j.rootCategory=INFO/log4j.rootCategory=WARN/g' ./log4j.properties
+cd
+sudo mv spark-* /usr/local/spark
+cd
+export SPARK_HOME=/usr/local/spark
+export SPARK_LOCAL_IP="127.0.0.1"
+export PATH=$PATH:$SPARK_HOME/bin
+echo export SPARK_HOME=/usr/local/spark >> ~/.profile
+echo export SPARK_LOCAL_IP="127.0.0.1" >> ~/.profile
+echo export PATH=$PATH:$SPARK_HOME/bin >> ~/.profile
+sudo chown -R ubuntu $SPARK_HOME
+
+
+echo "============================Install Docker==============================="
+# as per documentations of docker
+# make sure apt works with http
+cd
+
+sudo apt-get update
+
+sudo apt-get -y install bridge-utils
+
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+cd
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > docker.list
+sudo mv ./docker.list /etc/apt/sources.list.d/
+
+sudo apt-get update
+sudo apt-get purge lxc-docker
+sudo apt-cache policy docker-engine
+
+sudo apt-get update
+sudo apt-get install -y docker-engine
+sudo service docker start
+
+
+echo "=========================Create bridge==================================="
+##create bridge to avoid conflict with ip
+
+sudo service docker stop
+sudo ip link set dev docker0 down
+sudo brctl delbr docker0
+sudo iptables -t nat -F POSTROUTING
+sudo brctl addbr bridge0
+sudo ip addr add 192.168.5.1/24 dev bridge0
+sudo ip link set dev bridge0 up
+echo 'DOCKER_OPTS="-b=bridge0"' > docker
+sudo mv ./docker /etc/default/
+sudo service docker start
+sudo iptables -t nat -L -n
+
+echo "==========================Add username to docker group=================================="
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
+echo "==========================Clean up======================================="
+# Clean-up
+cd
+rm scala-2.11.8.deb
+rm spark-1.6.1-bin-hadoop2.6.tgz
+rm StarterScript.sh
+
+echo "=========================Docker Image======================"
+# this is because we want to be able to update the user's grop association witout
+# having to log out or without creating a sub-shell which is what newgrp does.
+sudo sg docker -c "docker pull ubuntu:14.04 && docker build -t sahandha/ubuntu:14.04 -t sahandha/ubuntu:latest ."
